ec966c8d1d246377bec6597041735050ad92565e
Su-Yong <simssy2205@gmail.com>
2020-06-14 23:22:02 +0900

    change main activity to room activity

--- app/src/main/java/io/suyong/linux_kakaotalk_client_android/MainActivity.kt
+++ app/src/main/java/io/suyong/linux_kakaotalk_client_android/MainActivity.kt
@@ -2,25 +2,11 @@ package io.suyong.linux_kakaotalk_client_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import io.suyong.linux_kakaotalk_client_android.room.Room
-import io.suyong.linux_kakaotalk_client_android.room.RoomListAdapter
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var adapter = RoomListAdapter(this)
-        room_list.adapter = adapter
-        room_list.layoutManager = LinearLayoutManager(this)
-        room_list.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
-
-        fab_connect.setOnClickListener {
-            adapter.list.add(Room("nothing", "test-title ${(Math.random() * 100).toInt()}", "test caption \n${Math.random()}", (Math.random() * 500).toInt()))
-            adapter.notifyDataSetChanged()
-        }
     }
 }
---
+++ app/src/main/java/io/suyong/linux_kakaotalk_client_android/network/NetworkManager.kt
@@ -0,0 +1,7 @@
+package io.suyong.linux_kakaotalk_client_android.network
+
+import io.socket.client.Socket
+
+class NetworkManager {
+    private var socket: Socket? = null
+}
